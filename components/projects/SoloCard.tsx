953a7f64485c34a352678ef059f5a9a920b0880e
William Vernon <wmvernon@proton.me>
2023-07-22 16:14:13 -0400

    feature- projects solocards

--- components/projects/SoloCard.tsx
+++ components/projects/SoloCard.tsx
@@ -19,7 +19,7 @@ export default function SoloCard({ header, link, tools, image }) {
 	return (
 		<Link
 			href={link}
-			className="py-3 px-5 max-w-lg my-2 mx-auto bg-[#3131313b]  flex items-center space-x-10 shadow-xl shadow-[0_15px_30px_-10px_rgb(255, 255, 255)] rounded-2xl"
+			className="py-3 px-5 w-full h-full my-2 mx-auto bg-[#3131313b]  flex items-center space-x-10 shadow-lg shadow-[0_15px_30px_-10px_rgba(255, 255, 255, 0)] rounded-2xl"
 		>
 			<motion.div
 				whileHover={{ scale: 1.02 }}
@@ -29,12 +29,12 @@ export default function SoloCard({ header, link, tools, image }) {
 					stiffness: 600,
 					damping: 5,
 				}}
-				className="p-4 max-w-lg my-1 mx-auto bg-[#181818a4]  flex items-center space-x-10 shadow-xl shadow-[0_15px_30px_-10px_rgb(255, 255, 255)] rounded-2xl"
+				className="flex items-center space-x-12 mx-auto  rounded-2xl"
 			>
-				<div className="pr-[0rem] md:pr-[1.5rem]">
+				<div className="pr-[0rem] pl-3">
 					<div className="text-xl flex items-center font-medium text-white">
 						<p> {header} </p>
-						<svg
+						{/* <svg
 							className="h-4 w-4 ml-1"
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
@@ -47,7 +47,7 @@ export default function SoloCard({ header, link, tools, image }) {
 								strokeWidth={2}
 								d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
 							/>
-						</svg>
+						</svg> */}
 					</div>
 					<p className="text-slate-400 text-xs">{tools}</p>
 				</div>
